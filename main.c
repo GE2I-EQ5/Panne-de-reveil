4b047c2dbe07f73fafa496b985b76a3376687957
plbrn <71700072+plbrn@users.noreply.github.com>
2020-09-23 14:53:12 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-  int m_reveil, h_reveil, m_bus, m_cours, h_cours;
-
+  int m_reveil, h_reveil, m_cours, h_cours;
+  int m_bus = 420; //7h 7*60
   _Bool frigo;
 
   printf("Le frigo est-il plein ? Entrez 1 pour plein et 0 pour vide : ");
@@ -16,23 +16,33 @@ int main()
 
   printf("Le cours est a : %d : %d\n", h_cours,m_cours);
 
-  m_cours = m_cours + h_cours*60-12
+  m_cours = m_cours + h_cours*60-12 ;
 
-  //printf("%d");
+  //printf("m_cours vaut %d\n", m_cours); Juste jusqu'ici
 
-  while(m_bus < m_cours)
+  while(m_bus <= m_cours)
   {
     m_bus +=11;
   }
 
   m_bus -=11;
 
+  //
+  printf("%d\n", m_bus);
+
   if(frigo == 1) //Si frigo plein
   {
     m_reveil = m_bus - 25;
   }
-  else
+  else //Sinon frigo vide
   {
     m_reveil = m_bus - 30;
   }
+
+  h_reveil = m_reveil/60;
+
+  m_reveil = m_reveil%60;
+
+  printf("Le reveil doit sonner a %d heures et %d minutes.", h_reveil, m_reveil);
+
 }
